ad061825600b61282953e3d7c0c8ab20f6ebbb4a
karamaru-alpha <mrnk3078@gmail.com>
2023-05-06 23:49:38 +0900

    a

--- sql/tenant/10_schema.sql
+++ sql/tenant/10_schema.sql
@@ -33,12 +33,3 @@ CREATE TABLE player_score (
   updated_at BIGINT NOT NULL,
   INDEX (`tenant_id`, `competition_id`, `player_id`)
 );
-
-DROP TABLE IF EXISTS `latest_visit_history`;
-CREATE TABLE `latest_visit_history` (
-    `tenant_id` BIGINT UNSIGNED NOT NULL,
-    `competition_id` VARCHAR(255) NOT NULL,
-    `player_id` VARCHAR(255) NOT NULL,
-    `created_at` BIGINT NOT NULL,
-    PRIMARY KEY  (`tenant_id`, `competition_id`, `player_id`)
-) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
