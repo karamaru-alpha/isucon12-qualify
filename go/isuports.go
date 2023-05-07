dd7ba6c3dfc459f0db7d09f2bc8f274c40d3bb08
karamaru-alpha <mrnk3078@gmail.com>
2023-05-07 20:23:11 +0900

    a

--- go/isuports.go
+++ go/isuports.go
@@ -1253,24 +1253,15 @@ func playerHandler(c echo.Context) error {
 	}
 
 	pss := make([]PlayerScoreRow, 0, len(cs))
-	for _, c := range cs {
-		ps := PlayerScoreRow{}
-		if err := tenantDB.GetContext(
-			ctx,
-			&ps,
-			// 最後にCSVに登場したスコアを採用する = row_numが一番大きいもの
-			"SELECT * FROM player_score WHERE tenant_id = ? AND competition_id = ? AND player_id = ? LIMIT 1",
-			v.tenantID,
-			c.ID,
-			p.ID,
-		); err != nil {
-			// 行がない = スコアが記録されてない
-			if errors.Is(err, sql.ErrNoRows) {
-				continue
-			}
-			return fmt.Errorf("error Select player_score: tenantID=%d, competitionID=%s, playerID=%s, %w", v.tenantID, c.ID, p.ID, err)
-		}
-		pss = append(pss, ps)
+	if err := tenantDB.GetContext(
+		ctx,
+		&pss,
+		// 最後にCSVに登場したスコアを採用する = row_numが一番大きいもの
+		"SELECT * FROM player_score WHERE tenant_id = ? AND player_id = ?",
+		v.tenantID,
+		p.ID,
+	); err != nil {
+		return fmt.Errorf("error Select player_score: tenantID=%d, competitionID=%s, playerID=%s, %w", v.tenantID, c.ID, p.ID, err)
 	}
 
 	cMap := make(map[string]CompetitionRow, len(cs))
@@ -1286,8 +1277,7 @@ func playerHandler(c echo.Context) error {
 		//}
 		psds = append(psds, PlayerScoreDetail{
 			CompetitionTitle: cMap[ps.CompetitionID].Title,
-
-			Score: ps.Score,
+			Score:            ps.Score,
 		})
 	}
 
